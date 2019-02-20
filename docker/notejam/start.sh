005f83a16c4eaeffa6c4fe5b6c30624b773232b2
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2019-02-21 00:42:46 +0100

    parametrized build and nginx configuration

--- docker/notejam/start.sh
+++ docker/notejam/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+envsubst '$SYMFONY_ENV,$SYMFONY__DATABASE__DRIVER,$SYMFONY__DATABASE__HOST,$SYMFONY__DATABASE__PORT,$SYMFONY__DATABASE__NAME,$SYMFONY__DATABASE__USER,$SYMFONY__DATABASE__PASSWORD' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf
+su -s /bin/bash www -c 'cd /www/code; php app/console doctrine:schema:update --force'
+touch /www/code/app/logs/prod.log
+chown www:www /www/code/app/logs/prod.log
 php-fpm7
 nginx
-tail  -f /var/log/nginx/notejam_access.log
+tail  -f /www/code/app/logs/prod.log
