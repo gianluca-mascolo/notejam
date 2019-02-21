31ecba48ec8793f15c5ae573c8431cedf61c639d
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2019-02-21 15:57:47 +0100

    build with docker compose using the correct db network. refined parameters in compose file

--- docker/notejam/start.sh
+++ docker/notejam/start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if ( [ -z ${SYMFONY_ENV+x} ] || [ -z ${SYMFONY__DATABASE__DRIVER+x} ] || [ -z ${SYMFONY__DATABASE__HOST+x} ] || [ -z ${SYMFONY__DATABASE__PORT+x} ] || [ -z ${SYMFONY__DATABASE__NAME+x} ] || [ -z ${SYMFONY__DATABASE__USER+x} ] || [ -z ${SYMFONY__DATABASE__PASSWORD+x} ] ); then {
+ echo "Missing parameters"
+ exit 1
+}
+fi
+
 envsubst '$SYMFONY_ENV,$SYMFONY__DATABASE__DRIVER,$SYMFONY__DATABASE__HOST,$SYMFONY__DATABASE__PORT,$SYMFONY__DATABASE__NAME,$SYMFONY__DATABASE__USER,$SYMFONY__DATABASE__PASSWORD' < /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf
 su -s /bin/bash www -c 'cd /www/code; php app/console doctrine:schema:update --force'
 touch /www/code/app/logs/prod.log
